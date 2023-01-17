294fe90441ca1144afd5b0c6bf27f87500d90464
Nuttawut <nuttawut.cariva@gmail.com>
2023-01-17 14:34:17 +0700

    - init project file.

---
+++ CRStepCounting/Classes/CRStepCounting.swift
@@ -0,0 +1,17 @@
+//
+//  CRStepCounting.swift
+//  CRStepCounting
+//
+//  Created by UnRafe on 13/1/2566 BE.
+//
+
+import Foundation
+
+class CRStepCounting {
+    
+    func fetchCountingSteps() -> String {
+        
+        return "10,475 steps"
+        /// Code counting
+    }
+}
